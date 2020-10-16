41a5f8ac2c9a6992ee6c76bb56f2a3fdbbd6d62e
Bryan Kwong <bfkwong@calpoly.edu>
2020-10-16 16:14:56 -0700

    Updates

---
+++ precache-manifest.1a24e867f78c32ca8b425ebdbd71bf39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b25510abab22647c006a9aa8b05d3c1",
+    "url": "/tofu-ide/index.html"
+  },
+  {
+    "revision": "f202a355692dcc1f722b",
+    "url": "/tofu-ide/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "65e8d716ead6547646ed",
+    "url": "/tofu-ide/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "f202a355692dcc1f722b",
+    "url": "/tofu-ide/static/js/2.0ea5af79.chunk.js"
+  },
+  {
+    "revision": "2c567677fc35b313142a0f3665a749ef",
+    "url": "/tofu-ide/static/js/2.0ea5af79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e8d716ead6547646ed",
+    "url": "/tofu-ide/static/js/main.aaeee7d6.chunk.js"
+  },
+  {
+    "revision": "9740e02048a025491eaa",
+    "url": "/tofu-ide/static/js/runtime-main.e06f1408.js"
+  }
+]);
